fc8743f38d78288e932a39d8ca974a5366b9e401
Evisceration <eviscerationls@gmail.com>
2014-10-06 13:58:46 +0200

    DisplayColorCalibration: read min value from file
    Change-Id: I6cf3b17f043c6d8db3f748cfec70d5d9bbe6b04b

--- cmhw/org/cyanogenmod/hardware/DisplayColorCalibration.java
+++ cmhw/org/cyanogenmod/hardware/DisplayColorCalibration.java
@@ -16,10 +16,14 @@
 
 package org.cyanogenmod.hardware;
 
+import java.util.Scanner;
+import java.io.File;
+
 import org.cyanogenmod.hardware.util.FileUtils;
 
 public class DisplayColorCalibration {
     private static final String COLOR_FILE = "/sys/devices/platform/kcal_ctrl.0/kcal";
+    private static final String COLOR_MIN = "/sys/devices/platform/kcal_ctrl.0/kcal_min";
 
     public static boolean isSupported() {
         return true;
@@ -29,7 +33,14 @@ public class DisplayColorCalibration {
         return 255;
     }
     public static int getMinValue()  {
-        return 0;
+        int ret = 25;
+        try {
+            Scanner s = new Scanner(new File(COLOR_MIN));
+            ret = s.nextInt();
+            s.close();
+        } catch (Exception ex) { }
+
+        return ret;
     }
     public static int getDefValue() {
         return getMaxValue();
