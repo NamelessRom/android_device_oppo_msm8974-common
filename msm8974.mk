f834d6dc42c1f4b26fc2a7d89c00bbcbe1bb598c
jumoog <kilianvonpflugk@gmail.com>
2014-10-15 13:22:39 +0200

    we are bacon lovers
    Change-Id: I41cd45becf0829c4992d2714cb4dae67a702dc28

--- msm8974.mk
+++ msm8974.mk
@@ -270,7 +270,8 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.usb.host.xml:system/etc/permissions/android.hardware.usb.host.xml \
     frameworks/native/data/etc/android.software.sip.voip.xml:system/etc/permissions/android.software.sip.voip.xml \
     frameworks/native/data/etc/android.hardware.telephony.gsm.xml:system/etc/permissions/android.hardware.telephony.gsm.xml \
-    frameworks/native/data/etc/android.hardware.bluetooth_le.xml:system/etc/permissions/android.hardware.bluetooth_le.xml
+    frameworks/native/data/etc/android.hardware.bluetooth_le.xml:system/etc/permissions/android.hardware.bluetooth_le.xml \
+    $(LOCAL_PATH)/configs/com.cyngn.crispybacon.xml:system/etc/permissions/com.cyngn.crispybacon.xml
 
 # Device uses high-density artwork where available
 PRODUCT_AAPT_CONFIG := normal hdpi xhdpi xxhdpi
