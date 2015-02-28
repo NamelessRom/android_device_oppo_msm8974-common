d5d4de4ae60d0347c5c25579205b090f28559f6a
Alexander Martinz <eviscerationls@gmail.com>
2015-02-28 23:51:25 +0100

    brick-device: switch to stock thermal engine
    Change-Id: If2056a4007d018af1b0549f05a724906aa1082ab
    Signed-off-by: Alexander Martinz <eviscerationls@gmail.com>

--- rootdir/etc/brick.device.sh
+++ rootdir/etc/brick.device.sh
@@ -5,12 +5,5 @@
 PATH=/sbin:/vendor/bin:/system/sbin:/system/bin:/system/xbin
 export PATH
 
-busybox mount -o rw,remount /system
-
-# kill thermal-engine as we have a built-in kernel solution
-mv /system/bin/thermal-engine /system/bin/thermal-engine.bak 2> /dev/null
-
-busybox mount -o ro,remount /system
-
 # stop mpdecision as we have a built-in kernel solution
 stop mpdecision
