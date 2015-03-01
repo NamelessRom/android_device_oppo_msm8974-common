c0f2fac7cbcf44a1051b5250191abc2ce95a58eb
Alexander Martinz <eviscerationls@gmail.com>
2015-03-01 01:12:34 +0100

    (DO NOT MERGE) set kcal preset
    Change-Id: I36b22aebf321b7bff4d1e97fd1ad13f74ab9eaf4
    Signed-off-by: Alexander Martinz <eviscerationls@gmail.com>

--- rootdir/etc/brick.device.sh
+++ rootdir/etc/brick.device.sh
@@ -5,5 +5,15 @@
 PATH=/sbin:/vendor/bin:/system/sbin:/system/bin:/system/xbin
 export PATH
 
+# set kcal preset
+echo "1" > /sys/devices/platform/kcal_ctrl.0/kcal_enable
+echo "256 256 256" > /sys/devices/platform/kcal_ctrl.0/kcal
+echo "35" > /sys/devices/platform/kcal_ctrl.0/kcal_min
+echo "0" > /sys/devices/platform/kcal_ctrl.0/kcal_invert
+echo "0" > /sys/devices/platform/kcal_ctrl.0/kcal_hue
+echo "275" > /sys/devices/platform/kcal_ctrl.0/kcal_sat
+echo "251" > /sys/devices/platform/kcal_ctrl.0/kcal_val
+echo "258" > /sys/devices/platform/kcal_ctrl.0/kcal_cont
+
 # stop mpdecision as we have a built-in kernel solution
 stop mpdecision
