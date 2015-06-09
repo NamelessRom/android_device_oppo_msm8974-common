56fab4f3615ed6cd88387d7f3325363fe0d50c92
Sultanxda <sultanxda@gmail.com>
2015-06-09 16:11:30 -0700

    msm8974: Remove vsync phase offsets
    Change-Id: I02ff05d34a5252f7fafd3c03577aca5b1733b5ab

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -76,8 +76,6 @@ TARGET_USES_C2D_COMPOSITION := true
 TARGET_USES_ION := true
 OVERRIDE_RS_DRIVER := libRSDriver_adreno.so
 HAVE_ADRENO_SOURCE:= false
-VSYNC_EVENT_PHASE_OFFSET_NS := 7500000
-SF_VSYNC_EVENT_PHASE_OFFSET_NS := 5000000
 
 # Shader cache config options
 # Maximum size of the  GLES Shaders that can be cached for reuse.
