db1e8c6a7009cb07d52118f6b90ae886b66c9c19
Alexander Martinz <eviscerationls@gmail.com>
2015-11-06 20:49:22 +0100

    Revert "msm8974: always dexopt bootimg"
      * To be reverted once things have settled down
    
    This reverts commit 8837e3cefb62a4ed7f02f8374d53bd0569c711a4.
    
    Change-Id: Ibc7124de012d23888e82ce432c55f7443506bd32
    Signed-off-by: Alexander Martinz <eviscerationls@gmail.com>

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -146,9 +146,10 @@ EXTENDED_FONT_FOOTPRINT := true
 
 # Enable dexpreopt to speed boot time
 ifeq ($(HOST_OS),linux)
-  ifeq ($(WITH_DEXPREOPT),)
-    WITH_DEXPREOPT := true
-    WITH_DEXPREOPT_BOOT_IMG_ONLY := true
+  ifeq ($(call match-word-in-list,$(TARGET_BUILD_VARIANT),user),true)
+    ifeq ($(WITH_DEXPREOPT),)
+      WITH_DEXPREOPT := true
+    endif
   endif
 endif
 
