30604e5f1ffdf07a7f28a2988d66a5134b0543d6
Keith Mok <kmok@cyngn.com>
2016-02-03 10:15:34 -0800

    msm8974: remove wifi.supplicant_scan_interval in system.prop
    It is in overlay now, and default is 15000ms also.
    
    Change-Id: Iafb7eb8f5215831496d7f2b29064f977bc6cd23e

--- msm8974.mk
+++ msm8974.mk
@@ -210,7 +210,6 @@ PRODUCT_PROPERTY_OVERRIDES += \
     ro.data.large_tcp_window_size=true \
     telephony.lteOnGsmDevice=1 \
     wifi.interface=wlan0 \
-    wifi.supplicant_scan_interval=15 \
     ro.qualcomm.perf.cores_online=2 \
     ro.telephony.call_ring.multiple=false \
     ro.telephony.call_ring.delay=5000 \
